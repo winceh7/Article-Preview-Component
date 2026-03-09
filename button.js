dfcf97ce0a53993b7daeba45e179cfea91f857bd
JP Suarez <112521606+winceh7@users.noreply.github.com>
2026-03-09 15:16:19 -0600

    add ARIA for toggletip

--- button.js
+++ button.js
@@ -4,7 +4,10 @@ const shareMenu = document.getElementById('share-options');
 shareBtn.addEventListener('click', (e) => {
     if(shareMenu.style.display === 'none' || shareMenu.style.display === '') {
         shareMenu.style.display = 'flex';
+        const isExpanded = shareBtn.getAttribute('aria-expanded') === 'true';
+        shareBtn.setAttribute('aria-expanded', !isExpanded);
     } else {
         shareMenu.style.display = 'none';
+        shareBtn.setAttribute('aria-expanded', 'false');
     }
 });
